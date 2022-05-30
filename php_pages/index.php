ca7c48455fe5567054f64db11053539414c84dbb
Rugile <rugilestasionyte@gmail.com>
2022-05-31 00:29:11 +0300

    home-page

--- php_pages/index.php
+++ php_pages/index.php
@@ -5,13 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/header.css">
+    <link rel="stylesheet" href="../styles/index.css">
     <title>Home</title>
 </head>
 <body>
-    
     <?php 
     define('KEY', 1);
     require 'C:/xampp/htdocs/bit/3-bank/php_components/header.php';
     ?>
+    <div class="moto">
+        <h1>Taupyk kartu su <span>Savers bank</span></h1>
+        <img class="piggy" src="../images/piggy.png" alt="Pig-icon">
+    </div>
+    
 </body>
 </html>
