ae141460f0e7e4121b6b24a4acacc7999c634751
Rugile <rugilestasionyte@gmail.com>
2022-05-30 10:51:36 +0300

    header html-css

---
+++ php_components/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/header.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="header-nav">
+        <div class="nav-logo">
+            <img src="../images/pig-icon.png" alt="Pig_icon"></img>
+            <span>Savers bank</span>
+        </div>
+        <div class="nav-links">
+        <a class="nav-link" href="#">Sąskaitų sąrašas</a>
+        <a class="nav-link" href="#">Sukurti naują sąskaitą</a>
+        <a class="nav-link" href="#">Pridėti lėšas</a>
+        <a class="nav-link" href="#">Nuskaičiuoti lėšas</a>
+        <div>
+
+    </nav>
+</body>
+</html>
